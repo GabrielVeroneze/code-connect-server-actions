64930fa84d62525109a392056134c69a477b9e77
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-11 17:06:39 -0300

    ♻️ refactor: envolve logo com link para a página inicial

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,15 +1,18 @@
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from './Aside.module.css'
 
 export const Aside = () => {
     return (
         <aside className={styles.aside}>
-            <Image
-                src="/images/logo.png"
-                alt="Logo da Code Connect"
-                height={40}
-                width={128}
-            />
+            <Link href="/">
+                <Image
+                    src="/images/logo.png"
+                    alt="Logo da Code Connect"
+                    height={40}
+                    width={128}
+                />
+            </Link>
         </aside>
     )
 }
