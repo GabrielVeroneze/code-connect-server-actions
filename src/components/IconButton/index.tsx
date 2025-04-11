549d4b423728147740ac183c8c5d3c31c2e8b132
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-11 20:04:51 -0300

    ✨ feat: cria componente IconButton com suporte a ícones

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './IconButton.module.css'
+
+interface IconButtonProps {
+    icon: string
+    alt: string
+}
+
+export const IconButton = ({ icon, alt, ...rest }: IconButtonProps) => {
+    return (
+        <button className={styles.botao} {...rest}>
+            <Image src={icon} alt={alt} height={24} width={24} />
+        </button>
+    )
+}
