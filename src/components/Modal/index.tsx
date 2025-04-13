8bfb244c30d163660fe6150824803640cc808383
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-13 17:09:30 -0300

    ✨ feat: cria componente Modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { forwardRef, useImperativeHandle, useRef } from 'react'
+import { ModalHandle } from '@/types/ModalHandle'
+import styles from './Modal.module.css'
+
+interface ModalProps {
+    children: React.ReactNode
+}
+
+export const Modal = forwardRef<ModalHandle, ModalProps>(({ children }, ref) => {
+    const dialogRef = useRef<HTMLDialogElement>(null)
+
+    const closeModal = () => {
+        dialogRef.current?.close()
+    }
+
+    const openModal = () => {
+        dialogRef.current?.showModal()
+    }
+
+    useImperativeHandle(ref, () => {
+        return {
+            closeModal,
+            openModal,
+        }
+    })
+
+    return (
+        <dialog className={styles.modal} ref={dialogRef}>
+            <header className={styles.cabecalho}>
+                <button className={styles.fechar}>X</button>
+            </header>
+            {children}
+        </dialog>
+    )
+})
+
+Modal.displayName = 'Modal'
