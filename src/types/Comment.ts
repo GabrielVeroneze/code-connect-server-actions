4d5d6358dabb94a4614da3635f6903317f81507f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-13 18:45:03 -0300

    ✨ feat: cria interface Comment

---
+++ src/types/Comment.ts
@@ -0,0 +1,11 @@
+import { Author } from '@/types/Author'
+import { Post } from '@/types/Post'
+
+export interface Comment {
+    id: number
+    text: string
+    author: Author
+    post: Post
+    parent?: Comment | null
+    children: Comment[]
+}
