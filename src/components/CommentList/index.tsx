e23ac4756cab1717b672728d4b3b3d2540f85a56
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-16 17:12:08 -0300

    ✨ feat: cria componente CommentList

---
+++ src/components/CommentList/index.tsx
@@ -0,0 +1,22 @@
+import { UserComment } from '@/components/UserComment'
+import { Comment } from '@/types/Comment'
+import styles from './CommentList.module.css'
+
+interface CommentListProps {
+    comments: Comment[]
+}
+
+export const CommentList = ({ comments }: CommentListProps) => {
+    return (
+        <section className={styles.secao}>
+            <h2 className={styles.titulo}>Comentários</h2>
+            <ul className={styles.lista}>
+                {comments.map(comment => (
+                    <li className={styles.item} key={comment.id}>
+                        <UserComment comment={comment} />
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
+}
