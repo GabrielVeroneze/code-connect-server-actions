4ce898b8ce35950349a7f0be2f7705de1835de31
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-19 17:18:13 -0300

    ✨ feat: exibe respostas do comentário com botão para alternar visibilidade

--- src/components/Replies/index.tsx
+++ src/components/Replies/index.tsx
@@ -1,9 +1,16 @@
 'use client'
 
 import { useState } from 'react'
+import { UserComment } from '@/components/UserComment'
+import { ModalReply } from '@/components/ModalReply'
+import { Comment } from '@/types/Comment'
 import styles from './Replies.module.css'
 
-export const Replies = () => {
+interface RepliesProps {
+    comment: Comment
+}
+
+export const Replies = ({ comment }: RepliesProps) => {
     const [showReplies, setShowReplies] = useState<boolean>(false)
 
     return (
@@ -14,8 +21,16 @@ export const Replies = () => {
             >
                 {showReplies ? 'Ocultar' : 'Ver'} respostas
             </button>
-            <ul className={styles.lista}>
-            </ul>
+            {showReplies && (
+                <ul className={styles.lista}>
+                    {comment.children?.map(reply => (
+                        <li className={styles.item} key={reply.id}>
+                            <UserComment comment={reply} />
+                            <ModalReply comment={reply} />
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
