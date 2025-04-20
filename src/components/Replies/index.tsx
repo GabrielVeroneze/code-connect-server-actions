c6fcdca3a25b4d48a16737d5955e2023057ad8ac
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-20 18:04:05 -0300

    ♻️ refactor: busca respostas do comentário via API

--- src/components/Replies/index.tsx
+++ src/components/Replies/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { UserComment } from '@/components/UserComment'
 import { ModalReply } from '@/components/ModalReply'
 import { Comment } from '@/types/Comment'
@@ -13,6 +13,21 @@ interface RepliesProps {
 export const Replies = ({ comment }: RepliesProps) => {
     const [showReplies, setShowReplies] = useState<boolean>(false)
 
+    const [replies, setReplies] = useState<Comment[]>([])
+
+    useEffect(() => {
+        if (showReplies) {
+            async function fetchReplies() {
+                const response = await fetch(`/api/comment/${comment.id}/replies`)
+                const data = await response.json()
+
+                setReplies(data)
+            }
+
+            fetchReplies()
+        }
+    }, [showReplies, comment.id])
+
     return (
         <div className={styles.container}>
             <button
@@ -23,7 +38,7 @@ export const Replies = ({ comment }: RepliesProps) => {
             </button>
             {showReplies && (
                 <ul className={styles.lista}>
-                    {comment.children?.map(reply => (
+                    {replies.map(reply => (
                         <li className={styles.item} key={reply.id}>
                             <UserComment comment={reply} />
                             <ModalReply comment={reply} />
